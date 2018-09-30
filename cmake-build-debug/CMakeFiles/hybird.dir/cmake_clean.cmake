66a203eb98266722cbf9cdeacd553ee38438f4d3
copyrightpoiiiii <36806487+copyrightpoiiiii@users.noreply.github.com>
2018-10-01 01:56:44 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/hybird.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hybird.dir/main.cpp.o"
+  "CMakeFiles/hybird.dir/P_G_C.cpp.o"
+  "CMakeFiles/hybird.dir/G_C.cpp.o"
+  "hybird.pdb"
+  "hybird"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hybird.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
